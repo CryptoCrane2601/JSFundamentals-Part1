966c462279bbb8e52eecc6fb39ef546de872e445
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-19 20:04:03 +0100

    assigment 1 done

---
+++ assignments.js
@@ -0,0 +1,7 @@
+let country = 'Serbia';
+let continent = 'Europe';
+let population = '6.834.000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- script.js
+++ script.js
@@ -4,5 +4,20 @@ console.log(40 + 8 + 23 - 10);
 console.log('Dejan');
 console.log(23);
 
+// Naming variables with camile case - JS is case sensitive
 let firstName = 'Dejan';
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $function = '27';
+let PI = 3.1415;
+let person = 'Dejan';
+
+// Good - Write good descriptive names for variables
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// Bad
+let job1 = 'Programmer';
+let job2 = 'Teacher';
