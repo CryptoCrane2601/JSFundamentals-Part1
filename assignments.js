463e5e04987f541f119641dfc2dc35f614ba2e5f
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-20 13:34:12 +0100

    finish challenge 1

--- assignments.js
+++ assignments.js
@@ -1,35 +1,35 @@
-//ASSIGMENTS:
+// //ASSIGMENTS:
 
-//Lecture 1 - Variables
-const country = 'Serbia';
-const continent = 'Europe';
-let population = 6834000;
+// //Lecture 1 - Variables
+// const country = 'Serbia';
+// const continent = 'Europe';
+// let population = 6834000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-//Lecture 2 - Data Types
-const isIsland = false;
-let language;
+// //Lecture 2 - Data Types
+// const isIsland = false;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
-//Lecture 3 - let, const and var
-language = 'Serbian';
+// //Lecture 3 - let, const and var
+// language = 'Serbian';
 
-// Lecture 4 - Basic operators
+// // Lecture 4 - Basic operators
 
-const halfCountry = population / 2;
-console.log(halfCountry);
-population++;
-console.log(population);
-const finlandPopulation = 6000000;
-console.log(country > finlandPopulation);
-const avgPopulation = 33000000;
-console.log(country > avgPopulation);
-const description =
-  'Serbia is in Europe, and its 6.8 million people speak serbian';
+// const halfCountry = population / 2;
+// console.log(halfCountry);
+// population++;
+// console.log(population);
+// const finlandPopulation = 6000000;
+// console.log(country > finlandPopulation);
+// const avgPopulation = 33000000;
+// console.log(country > avgPopulation);
+// const description =
+//   'Serbia is in Europe, and its 6.8 million people speak serbian';
