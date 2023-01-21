bd3ecd39ecd89b8af16129f3636a5b529879a05f
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-21 12:42:24 +0100

    clear the code

--- assignments.js
+++ assignments.js
@@ -1,40 +1,40 @@
-// //ASSIGMENTS:
+//ASSIGMENTS:
 
-// //Lecture 1 - Variables
-const country = 'Serbia';
-const continent = 'Europe';
-let population = 6834000;
+//Lecture 1 - Variables
+// const country = 'Serbia';
+// const continent = 'Europe';
+// let population = 6834000;
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+//console.log(country);
+//console.log(continent);
+//console.log(population);
 
-// //Lecture 2 - Data Types
-// const isIsland = false;
-// let language;
+//Lecture 2 - Data Types
+//const isIsland = false;
+//let language;
 
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+//console.log(typeof isIsland);
+//console.log(typeof population);
+//console.log(typeof country);
+//console.log(typeof language);
 
-// //Lecture 3 - let, const and var
-language = 'serbian';
+//Lecture 3 - let, const and var
+//language = 'serbian';
 
-// // Lecture 4 - Basic operators
+//Lecture 4 - Basic operators
 
-// const halfCountry = population / 2;
-// console.log(halfCountry);
-// population++;
-// console.log(population);
-// const finlandPopulation = 6000000;
-// console.log(country > finlandPopulation);
-// const avgPopulation = 33000000;
-// console.log(country > avgPopulation);
-// const description =
-//   'Serbia is in Europe, and its 6.8 million people speak serbian';
+//const halfCountry = population / 2;
+//console.log(halfCountry);
+//population++;
+//console.log(population);
+//const finlandPopulation = 6000000;
+//console.log(country > finlandPopulation);
+//const avgPopulation = 33000000;
+//console.log(country > avgPopulation);
+//const description =
+//'Serbia is in Europe, and its 6.8 million people speak serbian';
 
-// Lecture 5 - Strings and Template Literals
+//Lecture 5 - Strings and Template Literals
 
-const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`;
-console.log(description);
+//const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`;
+//console.log(description);
