bc0f8b2667d9879dbbe870ed64ca0db9ac7dbdeb
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-25 19:28:08 +0100

    complete challenge with if loop

--- script.js
+++ script.js
@@ -236,27 +236,46 @@
 
 // Lecture 11 - The Switch Statement:
 
-const day = 'monday';
+// const day = 'monday';
 
-switch (day) {
-  case 'monday':
-    console.log('Plan course structure');
-    console.log('Go to coding meetup');
-    break;
-  case 'tuesday':
-    console.log('Prepare theory videos');
-    break;
-  case 'wedneday':
-  case 'thursday':
-    console.log('Write code examples');
-    break;
-  case 'friday':
-    console.log('Record videos');
-    break;
-  case 'sutarday':
-  case 'sunday':
-    console.log('Enjoy the weekend');
-    break;
-  default:
-    console.log('Not a valid day!');
-}
+// switch (day) {
+//   case 'monday': // day === 'monday' - strict comparison
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wedneday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'sutarday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend');
+//     break;
+//   default:
+//     console.log('Not a valid day!');
+// }
+
+// Challenge :
+
+// const day = 'monday';
+
+// if (day === 'monday') {
+//   console.log('Plan course structure');
+//   console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//   console.log('Prepare theory videos');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//   console.log('Write code examples');
+// } else if (day === 'friday') {
+//   console.log('Record videos');
+// } else if (day === 'sutarday' || day === 'sunday') {
+//   console.log('Enjoy the weekend');
+// } else {
+//   console.log('Not a valid day!');
+// }
