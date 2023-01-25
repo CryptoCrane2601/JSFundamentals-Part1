f6681833a2a1ed5a238d0a0536a3c175820d1aea
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-25 19:37:13 +0100

    done assigment

--- assignments.js
+++ assignments.js
@@ -89,28 +89,28 @@
 
 // Lecture 10 - The switch Statement
 
-const language = 'serbian';
+// const language = 'serbian';
 
-switch (key) {
-  case 'chinese':
-  case 'mandarian':
-    console.log('MOST number of native speakers!');
-    break;
-  case 'spanish':
-    console.log('2nd place in number of native speakers');
-    break;
-  case 'english':
-    console.log('3rd place');
-    break;
-  case 'hindi':
-    console.log('Number 4');
-    break;
-  case 'arabic':
-    console.log('5th most spoken language');
-    break;
-  default:
-    console.log('Great language too :D');
-    break;
-}
+// switch (language) {
+//   case 'chinese':
+//   case 'mandarian':
+//     console.log('MOST number of native speakers!');
+//     break;
+//   case 'spanish':
+//     console.log('2nd place in number of native speakers');
+//     break;
+//   case 'english':
+//     console.log('3rd place');
+//     break;
+//   case 'hindi':
+//     console.log('Number 4');
+//     break;
+//   case 'arabic':
+//     console.log('5th most spoken language');
+//     break;
+//   default:
+//     console.log('Great language too :D');
+//     break;
+// }
 
 // Lecture 11 - The Conditional (Ternary) Operator
